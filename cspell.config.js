a83f0be7f7d1f4d65b9edbcc2ecaf3f3078660d8
Alexey Svetlichniy <dnebl00@gmail.com>
2023-06-19 01:31:41 +0300

    chore:  setting up spell checking

---
+++ cspell.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    $schema: 'https://raw.githubusercontent.com/streetsidesoftware/cspell/main/cspell.schema.json',
+    version: '0.2',
+    language: 'en',
+    useGitignore: true,
+    ignorePaths: ['pnpm-lock.yaml'],
+
+    // dictionaryDefinitions: [
+    //     {
+    //         name: 'local',
+    //         path: './.cspell/dict.txt',
+    //         addWords: true
+    //     }
+    // ],
+
+    // dictionaries: ['local'],
+
+    import: [
+        '@fruktorum/cspell-default-config/cspell-ext.json',
+        '@fruktorum/cspell-frontend-config/cspell-ext.json'
+    ]
+}
